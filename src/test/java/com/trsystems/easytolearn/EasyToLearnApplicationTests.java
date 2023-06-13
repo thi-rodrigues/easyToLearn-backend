2ca843af0cde91aec6f842ef8b490b5ef3c99398
thi-rodrigues <thiagorodrigues1203@outlook.com>
2023-06-13 09:55:32 -0300

    Projeto Iniciado

---
+++ src/test/java/com/trsystems/easytolearn/EasyToLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trsystems.easytolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyToLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
